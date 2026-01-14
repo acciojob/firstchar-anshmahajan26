86736e5a5da904d56e43981f096573871d75f03e
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-14 07:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	for (let i = 0; i < str.length; i++) {
+    // if character is not a space
+    if (str[i] !== " ") {
+      return str[i];
+    }
+  }
+  // if only spaces or empty string
+  return "";
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
